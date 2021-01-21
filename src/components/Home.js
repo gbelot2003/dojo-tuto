fb7e2fadc5a054c146a399bee2f0d085fdf51d89
Gerardo Belot <gerardo.belot@gmail.com>
2021-01-21 11:10:04 -0600

    Handle click events

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,19 @@
 
 export const Home = (title) => {
+
+    const handelClick = (e) => {
+        console.log("hello", e)
+    }
+
+    const handleAgain = (name, e) => {
+        console.log(`hello ${name}`, e)
+    }
+
     return (
         <div className="home">
             <h2>This is home</h2>
+            <button onClick={handelClick}>Click me</button>
+            <button onClick={(e) => { handleAgain("Mario", e) }}>Click me Again</button>
         </div>
     );
 }
