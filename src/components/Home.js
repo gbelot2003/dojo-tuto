f959fd8d2c31ec225a255f20046a5ebc0ab90acf
Gerardo Belot <gerardo.belot@gmail.com>
2021-01-21 11:44:30 -0600

    Usando array map para desplegar lista en la plantilla

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,20 @@
 import { useState } from 'react'
 export const Home = (title) => {
 
-    const [name, setName] = useState('Mario');
-    const [age, setAge] = useState(15);
-
-    const handelClick = (e) => {
-        setName('Gerardo')
-        setAge(30)
-    }
-
+    const [blogs, setBlogs] = useState([
+        { title: 'My new Blog', body: "loream ipsum...", author: "Gerardo", id: 1 },
+        { title: 'Welcome Party', body: "loream ipsum...", author: "Gerardo", id: 2 },
+        { title: 'New Dev Tipos', body: "loream ipsum...", author: "Gerardo", id: 3 },
+    ]);
     return (
         <div className="home">
-            <h2>This is home</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handelClick}>Click me</button>
+            <h2>Homepage</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
