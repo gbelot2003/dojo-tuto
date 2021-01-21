74b619034a30793ec5060af4e7267ed56a6bbac2
Gerardo Belot <gerardo.belot@gmail.com>
2021-01-21 11:18:19 -0600

    Trabajando con variables reactivas y estados

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,19 @@
-
+import { useState } from 'react'
 export const Home = (title) => {
 
-    const handelClick = (e) => {
-        console.log("hello", e)
-    }
+    const [name, setName] = useState('Mario');
+    const [age, setAge] = useState(15);
 
-    const handleAgain = (name, e) => {
-        console.log(`hello ${name}`, e)
+    const handelClick = (e) => {
+        setName('Gerardo')
+        setAge(30)
     }
 
     return (
         <div className="home">
             <h2>This is home</h2>
+            <p>{name} is {age} years old</p>
             <button onClick={handelClick}>Click me</button>
-            <button onClick={(e) => { handleAgain("Mario", e) }}>Click me Again</button>
         </div>
     );
 }
