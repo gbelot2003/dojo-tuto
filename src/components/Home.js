e0a78a456258994358fb81b3946c342b4919d5c8
Gerardo Belot <gerardo.belot@gmail.com>
2021-01-21 13:34:18 -0600

    configurando useEffect dependencies

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
 const Home = (title) => {
 
+    const [name, setName] = useState('mario');
+
     const [blogs, setBlogs] = useState([
         { title: 'My new Blog', body: "loream ipsum...", author: "Gerardo", id: 1 },
         { title: 'Welcome Party', body: "loream ipsum...", author: "Samuel", id: 2 },
@@ -16,12 +18,13 @@ const Home = (title) => {
 
     useEffect(() => {
         console.log("uses Effects ran")
-        console.log(blogs)
-    });
+        console.log(name)
+    }, [name]);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="Samuel Blogs" handleDelete={handleDelete} />
+            <button onClick={() => { setName("Gerardo") }}>Change Name</button>
+            <BlogList blogs={blogs} title={`${name} Blogs`} handleDelete={handleDelete} />
         </div>
     );
 }
