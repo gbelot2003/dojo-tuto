e1508c2e0dfaea6ce5dbd645b2a6657e28c9df75
Gerardo Belot <gerardo.belot@gmail.com>
2021-01-21 14:19:58 -0600

    Configurando manejo de errores

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,23 +2,35 @@ import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
 const Home = (title) => {
 
-
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if (!res.ok) {
+                    throw Error("Server error, the service could no fetch the data from resource")
+                }
                 return res.json();
             })
             .then((data) => {
                 console.log(data)
                 setBlogs(data)
+                setIsPending(false)
+            })
+            .catch(err => {
+                setError(err.message)
+                setIsPending(false)
             })
     }, []);
 
     return (
         <div className="home">
-            {blogs && <BlogList blogs={blogs} title={`Gerardo's Blogs`} />}
-        </div>
+            { error && <div style={{ "color": "red" }}>{error}</div>}
+            { isPending && <div>Loading...</div>}
+            { blogs && <BlogList blogs={blogs} title={`Gerardo's Blogs`} />}
+        </div >
     );
 }
 
