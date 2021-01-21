daa801f718934ddfb64a4644827cbadcfa0f37a0
Gerardo Belot <gerardo.belot@gmail.com>
2021-01-21 13:19:04 -0600

    pasando funciones como props a componentes

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,11 +6,16 @@ const Home = (title) => {
         { title: 'My new Blog', body: "loream ipsum...", author: "Gerardo", id: 1 },
         { title: 'Welcome Party', body: "loream ipsum...", author: "Samuel", id: 2 },
         { title: 'New Dev Tipos', body: "loream ipsum...", author: "Gerardo", id: 3 },
+        { title: 'Next Step Party', body: "loream ipsum...", author: "Samuel", id: 2 },
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
     return (
         <div className="home">
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'Samuel')} title="Samuel Blogs" />
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'Gerardo')} title="Gerardo's Homepage" />
+            <BlogList blogs={blogs} title="Samuel Blogs" handleDelete={handleDelete} />
         </div>
     );
 }
