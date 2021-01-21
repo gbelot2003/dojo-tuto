fce6853492286dc825e9bc7f29dba24e612d87d3
Gerardo Belot <gerardo.belot@gmail.com>
2021-01-21 12:28:34 -0600

    Filtrando Datos en BlogList individuales

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,13 @@ const Home = (title) => {
 
     const [blogs, setBlogs] = useState([
         { title: 'My new Blog', body: "loream ipsum...", author: "Gerardo", id: 1 },
-        { title: 'Welcome Party', body: "loream ipsum...", author: "Gerardo", id: 2 },
+        { title: 'Welcome Party', body: "loream ipsum...", author: "Samuel", id: 2 },
         { title: 'New Dev Tipos', body: "loream ipsum...", author: "Gerardo", id: 3 },
     ]);
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="Blogs Homepage" />
-
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Samuel')} title="Samuel Blogs" />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Gerardo')} title="Gerardo's Homepage" />
         </div>
     );
 }
